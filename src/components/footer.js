e92aff75305b60ccb838100d6184187f25ac6755
Alex Druhet <alex@listo.studio>
2022-05-23 18:27:19 +0200

    fix: update footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,13 +2,12 @@ import React from 'react'
 
 import Container from './container'
 import * as styles from './footer.module.css'
+import { siteMetadata } from '../../gatsby-config'
 
 const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
-      Built with <a href="https://contentful.com/">Contentful</a> and{' '}
-      <a href="https://gatsbyjs.com">Gatsby</a> &middot;{' '}
-      <a href="https://github.com/contentful/starter-gatsby-blog">Source</a>
+      © {new Date().getFullYear()} {siteMetadata.title} - {siteMetadata.description}
     </div>
   </Container>
 )

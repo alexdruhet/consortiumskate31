0935cf1d6592dd345604730d023331c8bd9c8c99
Alex Druhet <alex@listo.studio>
2022-05-26 19:40:05 +0200

    feat: add creative common licence badge ; fix: header logo width

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,6 +7,12 @@ import { siteMetadata } from '../../gatsby-config'
 const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
+      <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">
+        <img alt="Licence Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" />
+        </a><br />Ce(tte) œuvre est mise à disposition selon les termes de la 
+        <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Licence Creative Commons Attribution - Pas d&#39;Utilisation Commerciale - Pas de Modification 4.0 International</a>.
+    </div>
+    <div className={styles.container}>
       © {new Date().getFullYear()} {siteMetadata.title} - {siteMetadata.description}
     </div>
   </Container>

8f29b3eb3156a6b3eefb422267e6c071962c4e51
Alex Druhet <alex@listo.studio>
2022-06-11 17:38:29 +0200

    feat: wip: test contact form

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,36 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 import * as styles from './contact-form.module.css'
 
 const ContactForm = () => {
+  const [serverState, setServerState] = useState({
+    submitting: false,
+    status: null,
+  })
+  const handleServerResponse = (ok, msg, form) => {
+    setServerState({
+      submitting: false,
+      status: { ok, msg },
+    })
+    if (ok) {
+      form.reset()
+    }
+  }
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    setServerState({ submitting: true })
+    axios({
+      method: 'post',
+      url: 'https://server.consortiumskate31.org',
+      data: new FormData(form),
+    })
+      .then((r) => {
+        handleServerResponse(true, 'Merci!', form)
+      })
+      .catch((r) => {
+        handleServerResponse(false, r.response.data.error, form)
+      })
+  }
   return (
-    <form
-      className={styles.form}
-      method="post"
-      action="https://server.consortiumskate31.org"
-    >
-      <label className={styles.fieldName}>
-        Nom <input type="text" name="name" id="name" />
-      </label>
-      <label className={styles.fieldEmail}>
-        Email <input type="email" name="email" id="email" />
-      </label>
-      <label className={styles.fieldSubject}>
-        Sujet <input type="text" name="subject" id="subject" />
-      </label>
-      <label className={styles.fieldMessage}>
-        Message <textarea name="message" id="message" rows="5" />
-      </label>
-      <div className={styles.buttons}>
-        <button type="submit" className={styles.buttonSubmit}>
+    <div className={styles.formWrapper}> 
+      <form onSubmit={handleOnSubmit} className={styles.form}>
+        <div className={styles.group}>
+          <label for="email" required="required">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            aria-describedby="emailHelp"
+            placeholder="Votre email"
+            required="required"
+          />
+        </div>
+        <div className={styles.group}>
+          <label for="name" required="required">Nom</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Votre nom"
+            required="required"
+          />
+        </div>
+        <div className={styles.group}>
+          <label for="message" required="required">Message</label>
+          <textarea
+            rows="10"
+            name="message"
+            id="message"
+            placeholder="Votre message"
+            required="required"
+          />
+        </div>
+        <button
+          type="submit"
+          className="button"
+          disabled={serverState.submitting}
+        >
           Envoyer
         </button>
-        <input
-          type="reset"
-          value="Réinitialiser"
-          className={styles.buttonReset}
-        />
-      </div>
-    </form>
+        {serverState.status && (
+          <p className={!serverState.status.ok ? 'errorMsg' : ''}>
+            {serverState.status.msg}
+          </p>
+        )}
+      </form>
+    </div>
   )
 }
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,12 +2,14 @@ import React from 'react'
 import ContactForm from '../components/contact-form'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
+import Hero from '../components/hero'
 
 class ContactPage extends React.Component {
   render() {
     return (
       <Layout location={this.props.location}>
         <Seo title="Écrivez-nous" />
+        <Hero title="Écrivez-nous" />
         <ContactForm />
       </Layout>
     )
@@ -15,3 +17,4 @@ class ContactPage extends React.Component {
 }
 
 export default ContactPage
+

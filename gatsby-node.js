584f607d0c4d868d1776458f4e71ab9cb8e9f467
Alex Druhet <alex@listo.studio>
2022-06-12 18:30:08 +0200

    fix: demo tagged posts in posts nav

--- gatsby-node.js
+++ gatsby-node.js
@@ -67,18 +67,18 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
       const previousPostSlug =
         index === 0
           ? null
-          : posts[index + 1].tags &&
+          : posts[index + 1].tags !== null &&
             posts[index - 1].tags.includes('demo') &&
-            post.tags &&
+            post.tags !== null &&
             !post.tags.includes('demo')
           ? null
           : posts[index - 1].slug
       const nextPostSlug =
         index === posts.length - 1
           ? null
-          : posts[index + 1].tags &&
+          : posts[index + 1].tags !== null &&
             posts[index + 1].tags.includes('demo') &&
-            post.tags &&
+            post.tags !== null &&
             !post.tags.includes('demo')
           ? null
           : posts[index + 1].slug
